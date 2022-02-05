5b03038f3bcb7439d4bb8881084e98bea72aaad4
R0cker77 <3odyhayek@gmail.com>
2022-02-06 01:57:47 +0200

    added boxbutton class

--- main.py
+++ main.py
@@ -4,64 +4,67 @@ import asyncio
 from widgets.typing import Typing
 from widgets.timer import Timer
 from widgets.results import Results
+from widgets.boxbutton import BoxButton
 from util.textgenerator import get_text
 
 logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
 
 palette = [('netural', '', ''),
 		   ('wronginput', 'dark red',''),
-		   ('rightinput', 'dark green', '')
+		   ('rightinput', 'dark green', ''),
+		   ('highlight', 'black', 'dark green')
 ]
+
+# To do:
+# add the ability to change timer as user please
+# make defualt text if no connection is available
+# save user texts
+# save user records
+# add theme palettes
 
 class Main:
 	def __init__(self):
 		self.event_manager = asyncio.get_event_loop()
-		self.timer_componenet = Timer(timer=15)
+
+		self.timer_componenet = Timer(timer=10, align='center')
 		self.typing_component = Typing(get_text())
-		self.exit_button = urwid.Button('Quit')
-		self.reset_test = urwid.Button('Reset')
-		self.new_test = urwid.Button('New Test')
+		self.container_typing = urwid.LineBox(self.typing_component)
+		self.container_timer = urwid.LineBox(self.timer_componenet)
+
+		self.exit_button = BoxButton('Quit', on_press=self.exit_)
+		self.reset_test = BoxButton('Reset')
+		self.new_test = BoxButton('New Test', on_press=self._new_test)
 
 		self.buttons_col = urwid.Columns([self.exit_button, self.reset_test, self.new_test])
+		self.container_buttons_col = urwid.LineBox(self.buttons_col)
 
 
-		self.main_pile = urwid.Pile([self.timer_componenet, self.typing_component, self.buttons_col])
+		self.main_pile = urwid.Pile([self.container_timer, self.container_typing, self.container_buttons_col])
 
 		self.padding = urwid.Padding(self.main_pile, left=2, right=2)
 		self.filler = urwid.Filler(self.padding)
+
 		self.async_loop = urwid.AsyncioEventLoop(loop=self.event_manager)
 		self.urwid_loop = urwid.MainLoop(self.filler, palette, event_loop=self.async_loop)
 		
 		urwid.connect_signal(self.typing_component, 'change', self.type_checking)
-		urwid.connect_signal(self.exit_button, 'click', self.exit_)
-		urwid.connect_signal(self.new_test, 'click', self._new_test)
+
 
 	def type_checking(self, _, string_typed):
 		typing_status = self.typing_component.check_input(string_typed)
 		if(typing_status == True):
-			urwid.connect_signal(self.reset_test, 'click', self._reset_test)
+			self.reset_test.set_signal(self._reset_test)
 			self.timer_task = asyncio.create_task(self.timer_componenet.start_timer())
 			self.event_manager.create_task(self.timer_done())
 
 		elif(typing_status == False):
-			self.timer_task.cancel()
-			self.time = self.timer_componenet.cancel_timer()
-
-
-			self.buttons_col.widget_list = [self.exit_button]
-			self.main_pile.widget_list = [Results(self.typing_component.get_results(),
-									  self.time), self.buttons_col]
+			self.test_done()
 
 
 	async def timer_done(self):
 		await self.timer_task
 
-		self.timer_task.cancel()
-		self.time = self.timer_componenet.cancel_timer()
-
-		self.buttons_col.widget_list = [self.exit_button]
-		self.main_pile.widget_list = [Results(self.typing_component.get_results(),
-									  self.time), self.buttons_col]
+		self.test_done()
 
 	def _reset_test(self, *user_args):
 		urwid.disconnect_signal(self.typing_component, 'change', self.type_checking)
@@ -74,6 +77,8 @@ class Main:
 
 
 	def _new_test(self, user_args):
+		# Avoiding the exception isnt the best way..
+
 		urwid.disconnect_signal(self.typing_component, 'change', self.type_checking)
 		try:
 			self.timer_task.cancel()
@@ -86,6 +91,17 @@ class Main:
 		urwid.connect_signal(self.typing_component, 'change', self.type_checking)
 
 
+	def test_done(self):
+		self.timer_task.cancel()
+		self.time = self.timer_componenet.cancel_timer()
+
+		self.buttons_col.widget_list = [self.exit_button]
+
+		self.results_widget = urwid.Padding(Results(self.typing_component.get_results(), self.time), left=2, right=2)
+		self.container_results = urwid.LineBox(self.results_widget)
+
+		self.main_pile.widget_list = [self.container_results,
+									      self.container_buttons_col]
 
 	def exit_(self, *user_args):
 		raise urwid.ExitMainLoop()
--- widgets/timer.py
+++ widgets/timer.py
@@ -4,14 +4,15 @@ import asyncio
 
 logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
 
-class Timer(urwid.Text):
-	def __init__ (self, timer=60):
+class Timer(urwid.Edit):
+	def __init__ (self, timer=60, align='left'):
 		self.time_count = timer
-		super().__init__(f'Timer:{self.time_count}')
+		super().__init__(caption='Timer:',edit_text='15', align=align)
 	
 	def set_timer(self, timer):
 		self.time_count = timer
-		super().set_text(f'Timer:{self.time_count}')
+		super().set_edit_text(f'{self.time_count}')
+
 			
 	async def start_timer(self):
 		self.timer = self.time_count
@@ -20,11 +21,18 @@ class Timer(urwid.Text):
 				return
 
 			self.timer -= 1
-			super().set_text(f'Timer:{self.timer}')
+			super().set_edit_text(f'{self.time_count}')
 			await asyncio.sleep(1)		
 
 	def reset_timer(self):
-		super().set_text(f'Timer:{self.time_count}')
+		super().set_edit_text(f'{self.time_count}')
 
 	def cancel_timer(self):
 		return (self.time_count - self.timer)
+
+	def keypress(self, size, key):
+		allowed_chars = ('up', 'down', 'right', 'left',
+						 '0', '9', '8', '7', '6', '5',
+						 '4', '3', '2', '1', 'backspace', 'enter')
+
+		super().keypress(size, key)
