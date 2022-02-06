fbec720c67a518c21757abd665b7b9dafd49b53f
R0cker77 <3odyhayek@gmail.com>
2022-02-07 01:57:56 +0200

    Added Travis.ci configuration

--- tests/test_results_component.py
+++ tests/test_results_component.py
@@ -1,12 +1,12 @@
-from ..widgets.results import Results
+from ..util.calculate_util import calculate_acc, word_per_min
 
 
 def test_calculate_acc_returns_correct_values():
 
-    test_component_results = Results(checking_array=[
+    test_component_results = [
         True, True, True, True,
         True, True, True, False, True, True, True,
-        False, True, True, True, True], time=5)
+        False, True, True, True, True]
 
-    assert test_component_results.calculate_acc() == "87.50"
-    assert test_component_results.word_per_min() == "38.40"
+    assert calculate_acc(test_component_results) == 87.50
+    assert word_per_min(test_component_results, time=5) == 38
--- util/textgenerator.py
+++ util/textgenerator.py
@@ -1,12 +1,12 @@
 import requests
-
+from requests.exceptions import ConnectionError, ReadTimeout
 
 def get_text(num_sentences=3):
     metaphorpsum_url = f'http://metaphorpsum.com/sentences/{num_sentences}'
 
     try:
         response = requests.get(metaphorpsum_url, timeout=5)
-    except requests.exceptions.ConnectionError:
+    except ConnectionError:
         response = 'The quick brown fox jumps over the lazy dog'
         return response
 
